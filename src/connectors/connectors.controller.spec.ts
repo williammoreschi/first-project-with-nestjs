0e9c82d2d93b7d8146e02b4dcc124c9d1da9b36f
William Moreschi <williammoreschi@gmail.com>
2021-04-03 16:36:07 -0300

    Iniciado a criação de teste

--- src/connectors/connectors.controller.spec.ts
+++ src/connectors/connectors.controller.spec.ts
@@ -1,18 +1,47 @@
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConnectorsController } from './connectors.controller';
+import { ConnectorSchema } from './schemas/connector.schema';
+import { Connector } from './shared/connector';
+import { ConnectorService } from './shared/connector.service';
 
 describe('ConnectorsController', () => {
   let controller: ConnectorsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+        }),
+        MongooseModule.forRoot(
+          `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.notof.mongodb.net/db?retryWrites=true&w=majority`,
+        ),
+        MongooseModule.forFeature([
+          { name: 'Connector', schema: ConnectorSchema },
+        ]),
+      ],
       controllers: [ConnectorsController],
+      providers: [ConnectorService],
     }).compile();
 
     controller = module.get<ConnectorsController>(ConnectorsController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should be able to create a new connector', async () => {
+      const connector = <Connector>{
+        name: 'GooglE',
+        type: '',
+        privacy: '',
+        baseUrl: '',
+        logoUrl: '',
+        category: '',
+        description: '',
+        status: false,
+      };
+      expect(await controller.create(connector)).toHaveProperty('_id');
+    });
   });
 });
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,4 +1,9 @@
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserSchema } from './schemas/user.schema';
+import { User } from './shared/user';
+import { UserService } from './shared/user.service';
 import { UsersController } from './users.controller';
 
 describe('UsersController', () => {
@@ -6,13 +11,30 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          envFilePath: '.env.test',
+        }),
+        MongooseModule.forRoot(
+          `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.notof.mongodb.net/db?retryWrites=true&w=majority`,
+        ),
+        MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+      ],
       controllers: [UsersController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('create', () => {
+    it('should be able to create a new connector', async () => {
+      const user = <User>{
+        name: 'John Doe',
+        email: 'johndoe@email.com',
+        password: '147258369',
+      };
+      expect(await controller.create(user)).toHaveProperty('_id');
+    });
   });
 });
